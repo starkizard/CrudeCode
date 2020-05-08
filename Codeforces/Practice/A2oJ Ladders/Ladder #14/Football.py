8c153484daf35fcd03502a348a9e4e0d22d9ff37
Prabal Kalia <42800934+starkizard@users.noreply.github.com>
2020-05-08 22:02:56 +0530

    Add files via upload

---
+++ Codeforces/Practice/A2oJ Ladders/Ladder #14/Football.py
@@ -0,0 +1,7 @@
+# auth: starkizard
+# just checking if 1111111 present in input or 0000000 present in input.
+n=input()
+if "1111111" in n or "0000000" in n:
+    print("YES")
+else:
+    print("NO")
