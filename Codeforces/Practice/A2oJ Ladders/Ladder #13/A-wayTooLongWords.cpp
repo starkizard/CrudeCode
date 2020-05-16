92839be1a12637905053850dbb8c9e08cf50615d
Prabal Kalia <42800934+starkizard@users.noreply.github.com>
2020-05-16 11:46:02 +0530

    Add files via upload

---
+++ Codeforces/Practice/A2oJ Ladders/Ladder #13/A-wayTooLongWords.cpp
@@ -0,0 +1,46 @@
+// auth: starkizard
+// implementation problem.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define mod 1000000007
+#define f(i, a, b) for (long long i = a; i < b; i++)
+#define rep(i, a, b) for (long long i = a; i <= b; i++)
+#define fm(i, a, b) for (long long i = a; i > b; i--)
+#define repm(i, a, b) for (long long i = a; i >= b; i--)
+#define vi vector<int>
+#define vl vector<long long>
+#define vs vector<string>
+#define pqmaxi priority_queue<int>
+#define pqmaxl priority_queue<ll>
+#define pqmini priority_queue<int, vi, greater<int>>
+#define pqminl priority_queue<ll, vl, greater<ll>>
+#define all(x) x.begin(), x.end()
+#define test     \
+    ll test;     \
+    cin >> test; \
+    while (test--)
+#define pb(i) push_back(i)
+#define mp(i, j) make_pair(i, j)
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+int main()
+{
+    fast;
+    test{
+        string s;
+        cin >> s;
+        ll n = s.size();
+        if(n>10){
+            cout << s[0]<< n-2 <<s[n-1] <<"\n"; 
+        }
+        else{
+            cout << s <<"\n";
+        }
+    }
+    return 0;
+}
---
+++ Codeforces/Practice/A2oJ Ladders/Ladder #13/B-Drinks.cpp
@@ -0,0 +1,42 @@
+// auth: starkizard
+// answer would be sum of array /n
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define mod 1000000007
+#define f(i, a, b) for (long long i = a; i < b; i++)
+#define rep(i, a, b) for (long long i = a; i <= b; i++)
+#define fm(i, a, b) for (long long i = a; i > b; i--)
+#define repm(i, a, b) for (long long i = a; i >= b; i--)
+#define vi vector<int>
+#define vl vector<long long>
+#define vs vector<string>
+#define pqmaxi priority_queue<int>
+#define pqmaxl priority_queue<ll>
+#define pqmini priority_queue<int, vi, greater<int>>
+#define pqminl priority_queue<ll, vl, greater<ll>>
+#define all(x) x.begin(), x.end()
+#define test     \
+    ll test;     \
+    cin >> test; \
+    while (test--)
+#define pb(i) push_back(i)
+#define mp(i, j) make_pair(i, j)
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+int main()
+{
+    fast;
+    int n;
+    cin >> n;
+    vi p(n);
+    f(i,0,n){
+        cin >> p[i];
+    }
+    cout << setprecision(12) << (double)accumulate(all(p),0)/n << "\n";
+    return 0;
+}
