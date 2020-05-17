7f4a89b7aaa1a8dec4e3fac58721946423ba5cf0
starwiz-7 <54806954+starwiz-7@users.noreply.github.com>
2020-05-17 11:22:43 +0530

    Create flippingGame.py

---
+++ Codeforces/Practice/A2oJ Ladders/Ladder #14/flippingGame.py
@@ -0,0 +1,26 @@
+# Author:starwiz_7
+n = int(input())
+a = list(map(int, input().split()))
+
+mx = 0
+for i in range(n):
+    for j in range(n):
+
+        if j < i:
+            continue
+        # flip from i to j
+
+        cnt = 0
+        for k in range(i):
+            if a[k]:
+                cnt += 1
+        for k in range(i, j+1):
+            if not a[k]:
+                cnt += 1
+        for k in range(j+1, n):
+            if a[k]:
+                cnt += 1
+
+        mx = max(mx, cnt)
+
+print(mx)
