a99d87ee5027711392b903e8e812dbfeb61b51db
Prabal Kalia <42800934+starkizard@users.noreply.github.com>
2020-05-17 10:52:23 +0530

    Add files via upload

---
+++ Codeforces/Practice/A2oJ Ladders/Ladder #13/A-Presents.cpp
@@ -0,0 +1,45 @@
+// auth: starkizard
+// making an unordered map with input as it's key and index as it's value
+// print the values with keys starting from 1 to n
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define mod 1000000007
+#define f(i, a, b) for (long long i = a; i < b; i++)
+#define rep(i, a, b) for (long long i = a; i <= b; i++)
+#define fm(i, a, b) for (long long i = a; i > b; i--)
+#define repm(i, a, b) for (long long i = a; i >= b; i--)
+#define vi vector<int>
+#define vl vector<long long>
+#define vs vector<string>
+#define pqmaxi priority_queue<int>
+#define pqmaxl priority_queue<ll>
+#define pqmini priority_queue<int, vi, greater<int>>
+#define pqminl priority_queue<ll, vl, greater<ll>>
+#define all(x) x.begin(), x.end()
+#define test     \
+    ll test;     \
+    cin >> test; \
+    while (test--)
+#define pb(i) push_back(i)
+#define mp(i, j) make_pair(i, j)
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+int main()
+{
+    fast;
+    ll n,j; cin >> n;
+    unordered_map<ll,ll> umap;
+    f(i,1,n+1){
+        cin >> j;
+        umap[j]=i;
+    }
+    f(i,1,n+1){
+        cout << umap[i] << " ";
+    }
+    cout << "\n";
+    return 0;
+}
