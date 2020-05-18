12816368a012428491068133be819fb698612f97
Prabal Kalia <42800934+starkizard@users.noreply.github.com>
2020-05-18 22:48:19 +0530

    Add files via upload

---
+++ Codeforces/Practice/A2oJ Ladders/Ladder #13/A-boyOrGirl.cpp
@@ -0,0 +1,41 @@
+// auth: starkizard
+// putting all characters into a set then cecking if it's length is even or not
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define mod 1000000007
+#define f(i, a, b) for (long long i = a; i < b; i++)
+#define rep(i, a, b) for (long long i = a; i <= b; i++)
+#define fm(i, a, b) for (long long i = a; i > b; i--)
+#define repm(i, a, b) for (long long i = a; i >= b; i--)
+#define vi vector<int>
+#define vl vector<long long>
+#define vs vector<string>
+#define pqmaxi priority_queue<int>
+#define pqmaxl priority_queue<ll>
+#define pqmini priority_queue<int, vi, greater<int>>
+#define pqminl priority_queue<ll, vl, greater<ll>>
+#define all(x) x.begin(), x.end()
+#define test     \
+    ll test;     \
+    cin >> test; \
+    while (test--)
+#define pb(i) push_back(i)
+#define mp(i, j) make_pair(i, j)
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+int main()
+{
+    fast;
+    string s;cin >>s;
+    set<char> s1;
+    for(auto i:s){
+        s1.insert(i);
+    }
+    if(s1.size()%2==0) cout << "CHAT WITH HER!\n";
+    else cout << "IGNORE HIM!";
+    return 0;
+}
