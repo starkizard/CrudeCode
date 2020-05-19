1763598be2531ebf364e64b8780b34053810931e
Prabal Kalia <42800934+starkizard@users.noreply.github.com>
2020-05-19 20:16:24 +0530

    Add files via upload

---
+++ Codeforces/Practice/A2oJ Ladders/Ladder #13/A-HQ9+.cpp
@@ -0,0 +1,41 @@
+// auth: starkizard
+// only H , Q and 9 produce an output . + doesn't
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define mod 1000000007
+#define f(i, a, b) for (long long i = a; i < b; i++)
+#define rep(i, a, b) for (long long i = a; i <= b; i++)
+#define fm(i, a, b) for (long long i = a; i > b; i--)
+#define repm(i, a, b) for (long long i = a; i >= b; i--)
+#define vi vector<int>
+#define vl vector<long long>
+#define vs vector<string>
+#define pqmaxi priority_queue<int>
+#define pqmaxl priority_queue<ll>
+#define pqmini priority_queue<int, vi, greater<int>>
+#define pqminl priority_queue<ll, vl, greater<ll>>
+#define all(x) x.begin(), x.end()
+#define test     \
+    ll test;     \
+    cin >> test; \
+    while (test--)
+#define pb(i) push_back(i)
+#define mp(i, j) make_pair(i, j)
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+int main()
+{
+    fast;
+    string s; cin >> s;
+    int flag=0;
+    for(auto i:s){
+        if(i=='H' || i=='Q'|| i=='9') flag=1;
+    }
+    if(flag) cout << "YES\n";
+    else cout << "NO\n";
+    return 0;
+}
