26e078298d21f1dcb93e0b7a3f5a3cd8de6f117a
Prabal Kalia <42800934+starkizard@users.noreply.github.com>
2020-05-22 22:52:55 +0530

    Add files via upload

---
+++ Leetcode/30 Day Leetcoding Challenge/Week 2/diameterOfBinaryTree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# auth: starkizard
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        self.ans = 1
+        def depth(node):
+            if not node: return 0
+            L = depth(node.left)
+            R = depth(node.right)
+            self.ans = max(self.ans, L+R+1)
+            return max(L, R) + 1
+
+        depth(root)
+        return self.ans - 1
