561c3a704f2aab78f3bb3ddf330e2c932ffeb757
Hrishabh Joshi <54844760+joshi008@users.noreply.github.com>
2020-06-12 10:27:24 +0530

    Add files via upload

---
+++ Codeforces/Educational Codeforces Round 89/A-shovelAndSwords.cpp
@@ -0,0 +1,81 @@
+//# This can be done in O(1).
+//# my approach, let's say n for the greater value and k for the lesser value
+//# So we reduce the greater value by 2 and the lesser value by 1, until they become equal (they will if k doesn't run out)
+//# how much do we need to reduce, let's form an inequality
+//# n-2x=k-x
+//# gives x=n-k
+//# if k <n-k (that is , we will run out of the lesser items) , print k
+//# else we do get n-k steps from these opertions so we add it.
+//# now we'll get both our n and k equal
+//# we'll do one sword and one shovel to make them equal again
+//# both values reduce by 3 and for every such steps we get 2 emeralds
+//# so we add (k//3)*2 to our answer.
+//# now k becomes k%3 either 0,1 or 2
+//# if it's 2, we can have one more emerald.
+//# now print the answer
+
+#include <bits/stdc++.h>
+
+#define lli long long int
+#define mod 1000000007
+#define fi first
+#define se second
+#define ii pair<lli, lli>
+#define mp make_pair
+#define endl '\n'
+const double PI = 3.141592653589793238460;
+lli gcd(lli a, lli b) { if (a == 0) return b; return gcd(b % a, a);}
+lli max(lli a, lli b) {if (a > b) return a; else return b;}
+lli min(lli a, lli b) {if (a < b) return a; else return b;}
+lli power(lli a,lli b) {lli i,p=1; for(i=0;i<b;i++){p*=a;} return p;}
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+	
+	lli t;
+	
+	cin>>t;
+	
+	while(t--)
+	
+	{
+		
+		lli a,b,m1,m2,n=0,x,y,m;
+		
+		cin>>a>>b;
+		
+		x=a;
+		
+		y=b; 
+		
+		if(x>=2*y)
+		{
+			
+			n = y;
+		}
+		
+		else
+		
+		if(y>=2*x)
+		{
+			n = x;
+		}
+		
+		else
+		
+		{
+			n = (a+b)/3;
+		}
+		
+			
+		cout<<n<<endl;
+		
+	}
+	
+
+    return 0;
+}
+
---
+++ Codeforces/Educational Codeforces Round 89/C-palindromicPaths.cpp
@@ -0,0 +1,111 @@
+//# refer https://www.geeksforgeeks.org/zigzag-or-diagonal-traversal-of-matrix/ for diagonal order of a matrix
+//# Example: Rolling two dices would give us 36 pairs: (1,1),(1,2)...(6,6). Now visualize diagonal elements as pair having sum 2, pairs having sum 3 and so on.
+//# Therefore the diagonal elements of matrix are pairs having same sum.
+//# Given the word palindromic but what does it mean?
+//# Palindromic means that elements from (1,1) and (n,m) are same 1 or 0.
+//# When going from (1,1) we can go to either (1,2) or (2,1) so visualize this as diagonal with sum 3.
+//# At the same time we can go from (n,m) to (n-1,m) and (n,m-1)
+//# So question asks us to grab number of ways to make equidistant diagonal elements (from begin and end) equal.
+//# We have taken k as the maximum limit of diagonal sum.
+//# Run loop and see if i+j is p or is it the diagonal from the end (n+m-p)
+//# Add the minimum of zeroes or one's
+//# print the sum.
+
+#include <bits/stdc++.h>
+
+#define lli long long int
+#define mod 1000000007
+#define fi first
+#define se second
+#define ii pair<lli, lli>
+#define mp make_pair
+#define endl '\n'
+const double PI = 3.141592653589793238460;
+lli gcd(lli a, lli b) { if (a == 0) return b; return gcd(b % a, a);}
+lli max(lli a, lli b) {if (a > b) return a; else return b;}
+lli min(lli a, lli b) {if (a < b) return a; else return b;}
+lli power(lli a,lli b) {lli i,p=1; for(i=0;i<b;i++){p*=a;} return p;}
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+
+	lli t;
+	
+	cin>>t;
+	
+	while(t--)
+	{
+		
+		lli n,m,i,j,k,p,s=0,r=0,c=0;
+		
+		cin>>n>>m;
+		
+		lli a[n][m];
+		
+		k = m + n-1;
+		
+		for(i=0;i<n;i++)
+		{
+			
+			for(j=0;j<m;j++)
+			{
+				
+				cin>>a[i][j];
+				
+			}
+		}
+		
+		if(k%2==0)
+		{
+			
+			k = k/2;
+			
+		}
+		else
+		{
+			
+			k = (k+1)/2;
+			
+			k--;
+			
+		}
+		
+		for(p=2;p<=k+1;p++)
+		{
+			s=0;
+			
+			c=0;
+			
+			for(i=0;i<n;i++)
+			{
+				
+				for(j=0;j<m;j++)
+				{
+					
+					if((i+j+2==p || i+j+2==n+m-p+2 ))
+					{
+						
+						s += a[i][j];
+						
+						c++;
+						
+					}
+					
+				}
+				
+			}
+			
+			r += min(s,c-s);
+			
+		}
+		
+		cout<<r<<endl;
+		
+	}
+
+    return 0;
+}
+
