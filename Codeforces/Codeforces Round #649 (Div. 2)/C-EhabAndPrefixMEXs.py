e6ccac5c2ab16eab7608e65b44b319fe37d520a9
Prabal Kalia <42800934+starkizard@users.noreply.github.com>
2020-06-14 13:10:55 +0530

    Update C-EhabAndPrefixMEXs.py

--- Codeforces/Codeforces Round #649 (Div. 2)/C-EhabAndPrefixMEXs.py
+++ Codeforces/Codeforces Round #649 (Div. 2)/C-EhabAndPrefixMEXs.py
@@ -1,7 +1,5 @@
 # auth: starkizard
-# if a[i]> i+1 anytime , the answer would be -1. for example if our array is 1 1 4 4
-# TO have a minimum of 4 at index 2. we should have 0 1 2 3 earlier. There's not enough possible spaces for it.
-# else, We make an observation
+# We make an observation
 # whenever a[i]!=a[i-1], b[i] must be a[i-1]
 # and we'll fill the rest elements avoiding all elements of a in increasing order. (As we need to fulfil all the numbers (0,1,2,3) in case we want to have minimum 4
 
@@ -9,10 +7,6 @@ def main():
     #CODE GOES HERE:
     n=ii()
     a=li()
-    for i in range(n):
-        if a[i]>i+1:
-            print(-1)
-            return
     b=[-1]*n
     s=set(a)
     for i in range(1,n):
