276f76cd204729c20aea2c0cd2955b41de421fa1
Prabal Kalia <42800934+starkizard@users.noreply.github.com>
2020-07-07 10:15:32 +0530

    Create A-RequiredRemainder.py

---
+++ Codeforces/Codeforces Round #653 (Div. 3)/A-RequiredRemainder.py
@@ -0,0 +1,10 @@
+# auth: starkizard
+# pretty straightforward
+def main():
+    for t in range(ii()):
+        x,y,n=mi()
+        c=(n+ y-(n%x))
+        if c>n:
+            c-=x
+        print(c)
+ 
