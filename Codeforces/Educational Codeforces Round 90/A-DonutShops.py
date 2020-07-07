6f6b270a56a3ed58b6f62d11f0ce1071eddd1651
Prabal Kalia <42800934+starkizard@users.noreply.github.com>
2020-07-07 10:50:11 +0530

    Create A-DonutShops.py

---
+++ Codeforces/Educational Codeforces Round 90/A-DonutShops.py
@@ -0,0 +1,14 @@
+# auth: starkizard
+# simple solution, go through the code
+def main():
+    #CODE GOES HERE:
+    for t in range(ii()):
+        a,b,c=mi()
+        a0,a1=0,0
+        if c<=a:
+            print(-1,b)
+            continue
+        if c/b >=a:
+            print(1,-1)
+            continue
+        print(1,b)
