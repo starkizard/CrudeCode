6e06801eaf101d4f6ff9c4a30db5cff629a07397
Prabal Kalia <42800934+starkizard@users.noreply.github.com>
2020-07-07 10:33:44 +0530

    Create A-MagicalSticks.py

---
+++ Codeforces/Codeforces Round #654 (Div. 2)/A-MagicalSticks.py
@@ -0,0 +1,13 @@
+# auth: starkizard
+# for 1 to n,
+# we can make math.ceil(n/2) sticks of size n
+# combining 1 and n-1
+# combining 2 and n-2
+# and so on
+def main():
+    for t in range(ii()):
+        n=ii()
+        print(int(math.ceil(n/2)))
+ 
+ 
+                
