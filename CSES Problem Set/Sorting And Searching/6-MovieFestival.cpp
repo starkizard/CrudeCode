0e99d6259714558648480c4fae8865df1b24959e
Prabal Kalia <42800934+starkizard@users.noreply.github.com>
2020-07-07 11:48:24 +0530

    Create 6-MovieFestival.cpp

---
+++ CSES Problem Set/Sorting And Searching/6-MovieFestival.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define mod 1000000007
+#define f(i, a, b) for (long long i = a; i < b; i++)
+#define rep(i, a, b) for (long long i = a; i <= b; i++)
+#define fm(i, a, b) for (long long i = a; i > b; i--)
+#define repm(i, a, b) for (long long i = a; i >= b; i--)
+#define vi vector<int>
+#define vl vector<long long>
+#define vs vector<string>
+#define pqmaxi priority_queue<int>
+#define pqmaxl priority_queue<ll>
+#define pqmini priority_queue<int, vi, greater<int>>
+#define pqminl priority_queue<ll, vl, greater<ll>>
+#define all(x) x.begin(), x.end()
+#define test     \
+    ll test;     \
+    cin >> test; \
+    while (test--)
+#define pb(i) push_back(i)
+#define mp(i, j) make_pair(i, j)
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL)
+int main(){
+    fast;
+    ll n,start,end; cin >> n;
+    vector< pair< ll , ll >> jobs(n);
+    f(i,0,n){
+        cin >> start >> end;
+        jobs[i]={end,start};
+    }
+    sort(all(jobs));
+    ll count=0;
+    ll startat=0;
+    for(auto job: jobs){
+        if(job.second>=startat){
+            startat=job.first;
+            count+=1;
+        }
+    }
+    cout << count << "\n";
+
+}
