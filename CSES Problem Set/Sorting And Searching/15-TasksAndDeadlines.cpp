36ee5e3012366324af68b85702c078132267221c
Prabal Kalia <42800934+starkizard@users.noreply.github.com>
2020-07-23 20:02:05 +0530

    Create 15-TasksAndDeadlines.cpp

---
+++ CSES Problem Set/Sorting And Searching/15-TasksAndDeadlines.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define mod 1000000007
+#define f(i, a, b) for (long long i = a; i < b; i++)
+#define rep(i, a, b) for (long long i = a; i <= b; i++)
+#define fm(i, a, b) for (long long i = a; i > b; i--)
+#define repm(i, a, b) for (long long i = a; i >= b; i--)
+#define vi vector<int>
+#define vl vector<long long>
+#define vs vector<string>
+#define pqmaxi priority_queue<int>
+#define pqmaxl priority_queue<ll>
+#define pqmini priority_queue<int, vi, greater<int>>
+#define pqminl priority_queue<ll, vl, greater<ll>>
+#define all(x) x.begin(), x.end()
+#define test     \
+    ll test;     \
+    cin >> test; \
+    while (test--)
+#define pb(i) push_back(i)
+#define mp(i, j) make_pair(i, j)
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL)
+ 
+int main(){
+    ll n; cin >> n;
+    vector<pair<ll,ll>> v(n);
+    f(i,0,n){
+        ll a,b; cin >> a >> b;
+        v[i]={a,b};
+    }
+    sort(all(v));
+    ll answer=0;
+    ll time=0;
+    f(i,0,n){
+        time+=v[i].first;
+        answer+=v[i].second-time;
+    }
+    cout << answer << "\n";
+ 
+}
